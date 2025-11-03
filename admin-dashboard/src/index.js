c408c1a71007981ed0b687524c17058163a04d5a
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-11-03 09:30:55 +0200

    router

--- admin-dashboard/src/index.js
+++ admin-dashboard/src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 <Provider store={store}>  
-      <HashRouter basename='/admin-dashboard'>
+      <BrowserRouter basename='/admin-dashboard'>
         <App />
-      </HashRouter>
+      </BrowserRouter>
 </Provider>
   </React.StrictMode>
 );
